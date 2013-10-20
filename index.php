d77089bf8eb5e4c3a7d8e6c19a1b6778bd67d657
Victoria Louie <v.louie91@gmail.com>
2013-10-20 13:36:51 -0700

    UI tweaks

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
 <?php include 'main.php'; ?>
 <body>
 <div id="login">
-  username: <input type="text" name="username" size="15" />
-  password: <input type="password" name="password" size="15" />
-  <input type="submit" value="login" name="loginButton" />
+  <form method="POST" action="?">
+    username: <input type="text" name="username" size="15" />
+    password: <input type="password" name="password" size="15" />
+    <input type="submit" value="log in" name="loginButton" />
+    <input type="submit" value="register" name="registerButton" />
+  </form>
 </div>
-<div id="top" class="divstyle">
+<div id="top" class="divstyle shadow">
   <h2>TagTeam</h2>
   <input type="text" name="searchBox" size="25" />
   <input type="submit" value="search" name="searchButton" />
 </div>
 <div id="container">
-  <div id="content" class="divstyle">
-    <img src="http://www.grubgrade.com/wp-content/uploads/2009/08/Fluffernutter.jpg" />
+  <div id="content" class="divstyle shadow">
+    <img src="http://www.grubgrade.com/wp-content/uploads/2009/08/Fluffernutter.jpg" class="centered" />
   </div>
-  <div id="sidebar" class="divstyle">
+  <div id="sidebar" class="divstyle shadow">
     <h3>Upload</h3>
     <b>URL</b>
     <input type="text" name="imgUrl" />
