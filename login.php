aef0bcea061ff554af6aa90680d8981bd7284e7a
Victoria Louie <v.louie91@gmail.com>
2013-10-21 01:08:41 -0700

    Added log out functionality. User login system is complete, basically

--- login.php
+++ login.php
@@ -23,15 +23,26 @@
     $result = executeBoundSQL("select * from tag_user where user_name=:name and password=:password", $alltuples);
     $row = OCI_Fetch_Array($result, OCI_BOTH);
     if ($row){
-      var_dump($row);
+      //var_dump($row);
       $_SESSION['Username'] = $_POST['username'];
       $_SESSION['LoggedIn'] = 1;
-      echo "<meta http-equiv='refresh' content='2;index.php'>";
+      echo "<meta http-equiv='refresh' content='0;index.php'>";
       //header('Location: temp.php');
     }
   }
   elseif (array_key_exists('logoutButton', $_POST)){
-    //destroy session
+    //session_start();
+    $_SESSION = array();
+
+    if (ini_get("session.use_cookies")){
+      $params = session_get_cookie_params();
+      setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+      );
+    }
+    session_destroy();
+    echo "<meta http-equiv='refresh' content='0;index.php'>";
   }
  }
 ?>
